afd391abd0cac5fd4d79cf2a20e2e38f2c8fd0f7
tomahock <eu@tomahock.com>
2018-06-15 00:37:24 +0100

    search and removing 2017 items

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,44 +8,87 @@
         api = 'https://api.reckon.ai/',
         templates = {};
 
+    $('.js-shuffle-search').on('keyup', function (e) {
+        var searchText = e.target.value.toLowerCase();
+
+        if(searchText.length < 3){
+            return;
+        }
+
+        $('.d-none').removeClass('d-none');
+
+        $('section.active .aaa').each(function(e,t){
+            var title = $(this).data('title');
+
+            if(title.indexOf(searchText) === -1){
+                console.log('nio');
+                $(this).addClass('d-none');
+            }
+        })
+    });
+
     function getBeer() {
+        var beers = [];
         $.get(api + 'api/v1/priceUtils/catProducts?catName=cervejas', function(res) {
             console.log(res.data);
 
             for( i in res.data){
-                var timestamp = new Date(res.data[i].Prices[0].updatedAt);
-                res.data[i].timestamp = timestamp.getTime();
+                if(res.data[i].Prices[0].updatedAt.indexOf('2017') === -1){
+                    var timestamp = new Date(res.data[i].Prices[0].updatedAt);
+                    res.data[i].timestamp = timestamp.getTime();
+                    res.data[i].searchable = res.data[i].Product.name.toLowerCase();
+
+                    beers.push(res.data[i])
+                }
+
             }
 
-            $('#beer').html(Mustache.render(templates['items'],  res.data.sort(function (a, b) {
+            $('#beer').append(Mustache.render(templates['items'],  beers.sort(function (a, b) {
                 return b.timestamp - a.timestamp;
             }), {item: templates['item']}));
+
         });
     }
 
     function getOlive() {
+        var olives = [];
+
         $.get(api + 'api/v1/priceUtils/catProducts?catName=Tremoços,%20Azeitonas%20e%20Pickles', function(res) {
             for( i in res.data){
-                var timestamp = new Date(res.data[i].Prices[0].updatedAt);
-                res.data[i].timestamp = timestamp.getTime();
+                if(res.data[i].Prices[0].updatedAt.indexOf('2017') === -1){
+                    var timestamp = new Date(res.data[i].Prices[0].updatedAt);
+                    res.data[i].timestamp = timestamp.getTime();
+                    res.data[i].searchable = res.data[i].Product.name.toLowerCase();
+
+                    olives.push(res.data[i])
+                }
             }
 
-            $('#olive').html(Mustache.render(templates['items'],  res.data.sort(function (a, b) {
+            $('#olive').append(Mustache.render(templates['items'],  olives.sort(function (a, b) {
                 return b.timestamp - a.timestamp;
             }), {item: templates['item']}));
+
         });
     }
 
     function getChips() {
+        var chips = [];
+
         $.get(api + 'api/v1/priceUtils/catProducts?catName=Batatas%20Fritas%20e%20Aperitivos', function(res) {
             for( i in res.data){
-                var timestamp = new Date(res.data[i].Prices[0].updatedAt);
-                res.data[i].timestamp = timestamp.getTime();
+                if(res.data[i].Prices[0].updatedAt.indexOf('2017') === -1){
+                    var timestamp = new Date(res.data[i].Prices[0].updatedAt);
+                    res.data[i].timestamp = timestamp.getTime();
+                    res.data[i].searchable = res.data[i].Product.name.toLowerCase();
+
+                    chips.push(res.data[i])
+                }
             }
 
-            $('#chips').html(Mustache.render(templates['items'],  res.data.sort(function (a, b) {
+            $('#chips').append(Mustache.render(templates['items'],  chips.sort(function (a, b) {
                 return b.timestamp - a.timestamp;
             }), {item: templates['item']}));
+
         });
     }
 
